0225254e092a760207645d6db66dd3a5b80836ab
vfontirroig <vfontirroig@pm.me>
2026-06-28 19:23:48 +0100

    update GPU resource request and partition settings in run_train.sh

--- experiments/euclid-cosmos/run_train.sh
+++ experiments/euclid-cosmos/run_train.sh
@@ -5,9 +5,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16       # matches NUM_WORKERS in train.py
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:RTX8000:1        # request 1 RTX8000 GPU specifically
+#SBATCH --gres=gpu:1        # request any GPU 
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=compl      # change to your GPU partition
+#SBATCH --partition=compl,pscompl      # change to your GPU partition
 #SBATCH --chdir=/n03data/fontirro/galaxy-counter
 set -euo pipefail
 
