019b6a507936fc30ec5a2e768cc730b3d5052f7d
vfontirroig <vfontirroig@pm.me>
2026-07-06 16:54:11 +0100

    fix: update h5 file path for UMAP visualization script to use v4 dataset

--- experiments/euclid-cosmos/visualization/run_umap.sh
+++ experiments/euclid-cosmos/visualization/run_umap.sh
@@ -15,7 +15,7 @@ CKPT_DIR="/n03data/fontirro/checkpoints/euclid-cosmos-vis-f150w/test-4-phase1"
 
 python /n03data/fontirro/galaxy-counter/experiments/euclid-cosmos/visualization/umap_latent.py \
     --checkpoint "${CKPT_DIR}/best-epoch=218-step=195000.ckpt" \
-    --h5         "/n03data/fontirro/data_files/euclid_cosmos_pairs_vis_f150w.h5" \
+    --h5         "/n03data/fontirro/data_files/euclid_cosmos_pairs_v4.h5" \
     --out        "/n03data/fontirro/plots_model/euclid-cosmos-vis-f150w/test-4-phase1/umap_val.png" \
     --out-cutouts "/n03data/fontirro/plots_model/euclid-cosmos-vis-f150w/test-4-phase1/umap_val_cutouts.png" \
     --indices    "${CKPT_DIR}/val_indices.npy"
