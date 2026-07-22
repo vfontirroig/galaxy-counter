f7788a243c7d4e41d6d9643f2d6a5e4c99a77fa8
vfontirroig <vfontirroig@pm.me>
2026-07-22 23:24:59 +0100

    fix: update HDF5 path and checkpoint directory, increase number of training steps

--- experiments/euclid-cosmos/train.py
+++ experiments/euclid-cosmos/train.py
@@ -139,14 +139,14 @@ class EuclidCosmosModel(ConditionalFlowMatchingModule):
 # ---------------------------------------------------------------------------
 # CONFIG — edit before running
 # ---------------------------------------------------------------------------
-H5_PATH     = "/n03data/fontirro/data_files/euclid_cosmos_pairs_vis_f115w.h5"
-CKPT_DIR    = "/n03data/fontirro/checkpoints/euclid-cosmos-vis-f115w/test-1-phase1"  # where to save checkpoints and logs
+H5_PATH     = "/n03data/fontirro/data_files/euclid_cosmos_pairs_vis_f150w_v2.h5"
+CKPT_DIR    = "/n03data/fontirro/checkpoints/euclid-cosmos-vis-f150w/test-5-phase1"  # where to save checkpoints and logs
 
 BATCH_SIZE  = 64
 NUM_WORKERS = 16
 VAL_RATIO   = 0.05
 TEST_RATIO  = 0.001
-NUM_STEPS   = 200_000
+NUM_STEPS   = 250_000
 IMAGE_SIZE  = 64      #Cutout spatial size
 LR          = 1e-4    #learning rate for AdamW optimizer
 
