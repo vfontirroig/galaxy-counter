cf1a36614dba44c721fe24c4aceb60103dfcabca
vfontirroig <vfontirroig@pm.me>
2026-07-22 12:50:53 +0100

    fix: uncomment code to retrieve and print Euclid and COSMOS image sizes

--- experiments/euclid-cosmos/build_hdf5.py
+++ experiments/euclid-cosmos/build_hdf5.py
@@ -128,14 +128,14 @@ def main():
     print(f"Missing Euclid files: {len(missing_euclid)}")
     print(f"Missing COSMOS files: {len(missing_cosmos)}")
 
-    # euclid_paths = [os.path.join(EUCLID_DIR_PATH, p) for p in catalog[EUCLID_COL]]
-    # cosmos_paths = [os.path.join(COSMOS_DIR_PATH, p) for p in catalog[COSMOS_COL]]
-    # N = len(euclid_paths)
+    euclid_paths = [os.path.join(EUCLID_DIR_PATH, p) for p in catalog[EUCLID_COL]]
+    cosmos_paths = [os.path.join(COSMOS_DIR_PATH, p) for p in catalog[COSMOS_COL]]
+    N = len(euclid_paths)
 
-    # H_euc, W_euc = get_spatial_size(euclid_paths[0], EUCLID_HDU)
-    # H_cos, W_cos = get_spatial_size(cosmos_paths[0], COSMOS_HDU)
-    # print(f"Euclid image size : {H_euc} x {W_euc}")
-    # print(f"COSMOS image size : {H_cos} x {W_cos}")
+    H_euc, W_euc = get_spatial_size(euclid_paths[0], EUCLID_HDU)
+    H_cos, W_cos = get_spatial_size(cosmos_paths[0], COSMOS_HDU)
+    print(f"Euclid image size : {H_euc} x {W_euc}")
+    print(f"COSMOS image size : {H_cos} x {W_cos}")
 
 
     # # ------------------------------------------------------------------
