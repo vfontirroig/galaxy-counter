d5a0da84970c9a809ec36711f199bc9b80b32efc
vfontirroig <vfontirroig@pm.me>
2026-07-27 12:43:57 +0100

    fix: update output filenames in run_umap.sh for consistency

--- experiments/euclid-cosmos/visualization/run_umap.sh
+++ experiments/euclid-cosmos/visualization/run_umap.sh
@@ -17,6 +17,6 @@ PLOT_DIR="/n03data/fontirro/euclid-cosmos/plots_model/euclid-cosmos-vis-f150w/te
 python /n03data/fontirro/euclid-cosmos/galaxy-counter/experiments/euclid-cosmos/visualization/umap_latent.py \
     --checkpoint "${CKPT_DIR}/best-epoch=164-step=140000.ckpt" \
     --h5         "/n03data/fontirro/data_files/euclid_cosmos_pairs_vis_f150w_v2.h5" \
-    --out        "${PLOT_DIR}/umap_val.png" \
-    --out-cutouts "${PLOT_DIR}/umap_val_cutouts.png" \
+    --out        "${PLOT_DIR}/umap_test.png" \
+    --out-cutouts "${PLOT_DIR}/umap_test_cutouts.png" \
     --indices    "${CKPT_DIR}/test_indices.npy"
