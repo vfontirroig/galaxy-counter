82caf5d0f6cd8d18ed1bd51a9ea595371ee157ce
vfontirroig <vfontirroig@pm.me>
2026-09-10 12:13:51 +0100

    fix: update checkpoint and output filenames in UMAP visualization script

--- experiments/euclid-cosmos/visualization/run_umap.sh
+++ experiments/euclid-cosmos/visualization/run_umap.sh
@@ -15,8 +15,8 @@ CKPT_DIR="/n03data/fontirro/euclid-cosmos/checkpoints/euclid-cosmos-vis-f150w/te
 PLOT_DIR="/n03data/fontirro/euclid-cosmos/plots_model/euclid-cosmos-vis-f150w/test-7-phase1/v1"
 
 python /n03data/fontirro/euclid-cosmos/galaxy-counter/experiments/euclid-cosmos/visualization/umap_latent.py \
-    --checkpoint "${CKPT_DIR}/latest-step=step=116000.ckpt" \
+    --checkpoint "${CKPT_DIR}/best-epoch=79-step=97000.ckpt" \
     --h5         "/n03data/fontirro/data_files/euclid_cosmos_pairs_vis_f150w_v4.h5" \
-    --out        "${PLOT_DIR}/umap_test_2.png" \
-    --out-cutouts "${PLOT_DIR}/umap_test_cutouts_2.png" \
+    --out        "${PLOT_DIR}/umap_test_3.png" \
+    --out-cutouts "${PLOT_DIR}/umap_test_cutouts_3.png" \
     --indices    "${CKPT_DIR}/test_indices.npy"
